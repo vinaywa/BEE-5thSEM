26960ca71e0dfc70da08b1cc6454bb34ba309f56
vinay <waliarajput9@gmail.com>
2025-10-09 15:31:12 +0530

    Learning About Websockets and implementing Broadcasting,Ping Pong

---
+++ Lecture30/server.js
@@ -0,0 +1,41 @@
+const  { WebSocketServer } =require('ws');
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// wss.on("connection",function(socket){
+//     console.log("a new user connected");
+//     socket.send("welcome !!!")
+//     setInterval(()=>{
+//         socket.send("Reliance stock price is "+" "+Math.random())
+//     },1000)
+// })
+
+// wss.on("connection",function(socket){
+//     console.log("a new user connected");
+//     socket.send("welcome !!!")
+//     socket.on("message",function(msg){
+//              console.log(msg.toString())
+//              if(msg.toString()=="ping")
+//              {
+//                 socket.send("pong")
+//              }
+//              else{
+//                 socket.send("Not correct message")
+//              }
+//     })
+// })
+
+// broadcast
+let allSockets=[]
+wss.on("connection",function(socket){
+
+    console.log("user connected")
+    allSockets.push(socket)
+    // console.log(allSockets)
+    socket.on("message",function(message){
+        allSockets.forEach((n)=>{
+          n.send(message.toString())
+        })
+    })
+
+})
